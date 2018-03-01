adfac10f7836d21071f9a22b156af6f13a2db873
Diego Lopes Lima <web.diego.lima@gmail.com>
2018-03-01 16:45:04 -0300

    Enhancements.

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require('path');
 
 module.exports = [
 	{
+		mode: 'development',
+		target: 'node',
 		entry: path.join(__dirname, 'source/server.jsx'),
 		output: {
 			path: path.join(__dirname, 'distribution'),
 			filename: 'server.bundler.js'
 		},
-		target: 'node',
 		module: {
 			rules: [
 				{
@@ -25,6 +26,8 @@ module.exports = [
 		}
 	},
 	{
+		mode: 'development',
+		target: 'web',
 		entry: path.join(__dirname, 'source/home/home.client.jsx'),
 		output: {
 			path: path.join(__dirname, 'distribution/public/assets'),
